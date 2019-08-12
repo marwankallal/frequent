204017680f9722a0913c9f2ada687dc2f0b67d67
Marwan Kallal <marwan.kallal@gmail.com>
2019-08-12 15:55:39 -0400

    added new counter struct, updated init function to reflect this

--- src/freq.c
+++ src/freq.c
@@ -3,7 +3,7 @@
 
 // Set up the internals (link the lists etc)
 void init_freq(struct freq_s *freq){
-    // Set head and tail
+    // Set head and tail groups
     freq->g_head = &(freq->groups[0]);
     freq->g_tail = &(freq->groups[NUM_BINS - 1]);
     
@@ -11,12 +11,42 @@ void init_freq(struct freq_s *freq){
     // lots of checks when NUM_BINS is high
     freq->g_head->prev = NULL;
     freq->g_head->next = &(freq->groups[1]);
+    freq->g_head->count = 0;
     freq->g_tail->prev = &(freq->groups[NUM_BINS - 2]);
     freq->g_tail->next = NULL;
+    freq->g_tail->count = 0;
 
-    // Link the rest of the list
+    // Link the rest of the group list, make counter list
     for (int i = 1; i < NUM_BINS - 1; i++){
         freq->groups[i].next = &(freq->groups[i + 1]);
         freq->groups[i].prev = &(freq->groups[i - 1]);
+        freq->groups[i].count = 0;
+
+        // Make counter linked list
+        //Make head and tail
+        freq->groups[i].c_head = &(freq->groups[i].counters[0]);
+        freq->groups[i].c_head->next = &(freq->groups[i].counters[1]);
+        freq->groups[i].c_head->prev = NULL;
+        freq->groups[i].c_head->value = 0;
+        freq->groups[i].c_head->assigned = 0;
+        freq->groups[i].c_tail = &(freq->groups[i].counters[NUM_BINS - 1]);
+        freq->groups[i].c_tail->next = NULL;
+        freq->groups[i].c_tail->prev = &(freq->groups[i].counters[NUM_BINS - 2]);
+        freq->groups[i].c_tail->value = 0;
+        freq->groups[i].c_tail->assigned = 0;
+         
+        for(int k = 1; k < NUM_BINS - 1; k++){
+            freq->groups[i].counters[k].group = &(freq->groups[i]);
+            freq->groups[i].counters[k].next = &(freq->groups[i].counters[k + 1]);
+            freq->groups[i].counters[k].prev = &(freq->groups[i].counters[k - 1]);
+            freq->groups[i].counters[k].assigned = 0;
+        }
     }
+    
+
+}
+
+// Add UNSIGNED int to tracker
+void freq_add_int(struct freq_s *freq, int val){
+
 }
--- src/freq.h
+++ src/freq.h
@@ -1,13 +1,24 @@
+#include <inttypes.h>
+
 #define NUM_BINS 10 // The top NUM_BINS elements will be stored
+
+struct counter_s{
+    int value;
+    uint8_t assigned;
+    struct group_s *group;
+    struct counter_s *next;
+    struct counter_s *prev;
+};
 
 struct group_s{
     // Keep track of this groups count
-    float val;
     int count;
+    struct counter_s counters[NUM_BINS];
+    struct counter_s *c_head;
+    struct counter_s *c_tail;
     struct group_s *next;
     struct group_s *prev;
 };
-
 
 struct freq_s {
     struct group_s groups[NUM_BINS];
@@ -16,3 +27,5 @@ struct freq_s {
 };
 
 void init_freq(struct freq_s *freq);
+void freq_add_int(struct freq_s *freq, int val);
+void display_freq(struct freq_s *freq);
